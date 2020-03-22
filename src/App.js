b3b29d76b5b3b84a3978114f17063aa62e2e4eb9
fadilnatakusumah <fadil.ntksmh@gmail.com>
2020-03-22 15:32:00 +0700

    convert App js to class component

--- src/App.js
+++ src/App.js
@@ -17,24 +17,46 @@ const dummyData = [{
   onlineStatus: true
 }]
 
-function App({ getPosts, posts }) {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="App" data-test="app-component">
-      <Header />
-      <Headline
-        dummyData={dummyData}
-        title="Post title"
-        desc="This is post title"
-      />
-      <div className="wrapper">
-        <SharedButton buttonText="Get Posts" emitEvent={getPosts} />
-        {
-          posts.map(post => (<PostItem key={post.title} title={post.title} desc={post.body} />))
-        }
-      </div>
-    </div>
-  );
+    this.state = {
+      hideBtn: false,
+    }
+  }
+
+  exampleMethod_updateState = () => {
+    this.setState({ hideBtn: !this.state.hideBtn })
+  }
+
+  getPostsHandler = () => {
+    const { getPosts } = this.props;
+    getPosts();
+    this.exampleMethod_updateState();
+  }
+
+  render() {
+    const { posts } = this.props;
+    const { state, getPostsHandler } = this;
+
+    return (
+      <div className="App" data-test="app-component" >
+        <Header />
+        <Headline
+          dummyData={dummyData}
+          title="Post title"
+          desc="This is post title"
+        />
+        <div className="wrapper">
+          {!state.hideBtn && <SharedButton data-test="shared-button" buttonText="Get Posts" emitEvent={getPostsHandler} />}
+          {
+            posts.map(post => (<PostItem key={post.title} title={post.title} desc={post.body} />))
+          }
+        </div>
+      </div >
+    );
+  }
 }
 
 const mapStateToProps = state => ({
